1ffca3671ff820a4367863ab9d7332b0b21315a2
Jonathan Peters <72918619+QMS85@users.noreply.github.com>
2025-04-09 13:18:31 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+//BUTTON REDIRECT//
+document.addEventListener('DOMContentLoaded', function() {
+    // Select the button by its ID
+    const redirectButton = document.getElementById('redirectButton');
+
+    // Add a click event listener to the button
+    redirectButton.addEventListener('click', function() {
+        // Redirect the user to the specified URL
+        window.location.href = 'https://www.linkedin.com/in/2jonathanpeters';
+    });
+});
+//END OF BUTTON REDIRECT//
